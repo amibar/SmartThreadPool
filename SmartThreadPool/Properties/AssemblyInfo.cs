d621800e9ebebf4b783a29b2ff6850e07c6b5290
Ami Bar <amibar@gmail.com>
2009-12-24 01:42:27 +0200

    Update version to 2.2.1.0

--- SmartThreadPool/Properties/AssemblyInfo.cs
+++ SmartThreadPool/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Amib.Threading")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Smart Thread Pool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Amib.Threading")]
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c764a3de-c4f8-434d-85b5-a09830d1e44f")]
-[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.2.1.0")]
 [assembly: InternalsVisibleTo("STPTests")]
 
 
